02b7fe985b1823805cd5890bffb68189e3f47b96
James <bier@toast.net>
2015-06-03 16:48:46 -0400

    Uploading Plot2

---
+++ Plot2.R
@@ -0,0 +1,20 @@
+# Load data
+tab<-read.table("household_power_consumption.txt",header=TRUE,sep=";")
+
+# Subset data only from 2007-02-01 and 2007-02-02
+days<-c("1/2/2007","2/2/2007")
+# days<-as.Date(c("2007-02-01/2007","2007-02-02"))
+tab<-tab[tab$Date %in% days,]
+
+# Convert Date and Time from factors to POSIXlt
+# dates<-as.Date(tab$Date,format="%d/%m/%Y")
+# times<-strptime(tab$Time,format="%H:%M:%S")
+datetime<-paste(tab$Date,tab$Time)
+minutes<-strptime(datetime,format="%d/%m/%Y %H:%M:%S")
+
+# Plot 2
+png(file="Plot2.png")
+plot(minutes,as.numeric(as.character(tab$Global_active_power)),
+     type="l",
+     ylab="Global Active Power (kilowatts)")
+dev.off()
